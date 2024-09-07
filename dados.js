7fd1c613034976c98fbe4397d6c0172395ea6875
lefarias-med <leticiafariasdesouza3@gmail.com>
2024-09-07 14:06:07 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo: "Cefaleia",
+        descricao: "Cefaleia ou dor de cabeça é uma queixa extremamente comum e que a maioria das pessoas. Na grande maioria dos casos, trata-se de algo benigno ou primário, ou seja, algo que não é grave e não coloca a vida da pessoa em risco. As causas mais comuns são a dor de cabeça tensional e a enxaqueca/migrânea. <p>Porém, existe algumas causas graves que podem causar uma dor de cabeça secundária, como uma rotura de aneurisma ou meningite. Como não faz sentido tomografar todos os pacientes com dor de cabeça, as grandes sociedades médicas, baseadas em forte evidências e estudos, propõe uma série de “Sinais de Alarme” que, longe de serem capazes de dar um diagnóstico, servem para, como o nome indica, sinalizar já na avaliação inicial quais pacientes precisam de uma avaliação mais detalhada e, possivelmente, de exames complementares como a Tomografia. <p>Sinais de alarme: Mudança do padrão de dor; Piora progressiva da dor; Refratária ao tratamento; Início súbito; Thunderclap; Primeiro episódio de cefaleia intensa; Febre; Rash cutâneo; Rigidez de nuca; Emagrecimento; Imunossupressão; Neoplasia conhecida; Início após 50 anos; Déficit focal; Papiledema; Rebaixamento nível de consciência; Convulsão;PAS > 220 mm Hg;Cefaleia aos esforços;Alteração visual",
+        link: "https://sbcefaleia.com.br/images/file%205.pdf",
+        tags: "cefaleia,cabeça"
+      },
+      
+    {
+        titulo: "Dor abdominal",
+        descricao: "Dor de barriga é uma queixa extremamente comum e que a maioria das pessoas já teve em algum momento da vida. Na imensa maioria dos casos, trata-se de algo benigno, como ter comido algo que fez mal ou uma “virose”, que no jargão médico poderia ser uma GECA, Gastroenterite aguda. Porém, existe algumas causas graves que podem causar dor abdominal, como a apendicite e por isso a avaliação inicial deve focar na identificação de possíveis “Sinais de alarme”. <p>São eles: Sinais de má perfusão periférica e instabilidade hemodinâmica, Febre, Início súbito e com intensidade máxima de dor desde o início, Idade acima de 65 anos, Etilismo, Imunossupressão, Doença cardiovascular, Cirurgia abdominal recente, Início da gestação, Vômitos, Sinais de irritação peritoneal, Hemorragia digestiva, Sintomas urinários, Hematúria, Amenorreia, Massa anexial, Taquicardia, Hipotensão, Rigidez abdominal, Discordância entre pulsos, Sopro abdominal, Parada de flatos ou fezes, Dor intensa pós-prandial",
+        link: "https://www.hu.usp.br/wp-content/uploads/sites/176/2017/06/432_Funda_protocolo_clinico_e_de_regulacao_do_acesso_para_dor_abdominal.pdf",
+        tags: "abdominal,barriga"
+    },
+    {
+        titulo: "Dor lombar",
+        descricao: "Quem nunca teve uma dor nas costas? Pois é. Eu já tive várias e normalmente são apenas musculares mesmo, o famoso “mal jeito”. Porém, existem alguns “Sinais de Alarme” que, longe de serem capazes de dar um diagnóstico, servem para, como o nome indica, sinalizar já na avaliação inicial quais pacientes precisam de uma avaliação mais detalhada e, possivelmente, de exames complementares. <p>São eles: Idade inferior a 20 anos, Idade superior a 50 anos, Mecanismo de trauma de alta energia, Evento desencadeador presente e claudicação, História de neoplasia, Uso de drogas injetáveis, Febre persistente, Sudorese noturna, Infecção bacteriana recorrente ou bacteremia, Dor em cólica, hematúria, diaforese, e náuseas, Idade, náuseas, hipertensão, doença vascular periférica, Imunossuprimidos, Déficit neurológico motor, Retenção urinária, Incontinência fecal, Anestesia em sela.",
+        link: "https://amb.org.br/files/_BibliotecaAntiga/lombalgias-e-lombociatalgias.pdf",
+        tags: "lombar,lombalgia,costas"
+    }
+];
